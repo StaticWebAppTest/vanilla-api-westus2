7185b1651ef574ba3652ca973f582886f9db1e81
Annaji Sharma Ganti <42851022+annaji-msft@users.noreply.github.com>
2021-03-01 15:06:59 -0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaultss to 200 */
-    body: { text: "Helllo from the APIs" },
+    body: { text: "Hellslo from the APIs" },
   };
 };
