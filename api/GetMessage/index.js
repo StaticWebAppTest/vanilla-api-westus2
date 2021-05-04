012c44a0228b7c78e91cd8b1e1a284f1a89051e3
Annaji Sharma Ganti <42851022+annaji-msft@users.noreply.github.com>
2021-05-04 11:35:34 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaultss to 200 */
-    body: { text: "dfds from the APIs" },
+    body: { text: "dfdsh from the APIs" },
   };
 };
