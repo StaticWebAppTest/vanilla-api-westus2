49668afbf26b9ff0b42ce98aa15f3118f5b5d317
Annaji Sharma Ganti <42851022+annaji-msft@users.noreply.github.com>
2021-05-07 01:43:33 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,1 +1,1 @@
-module.exports = async function (context, req) {  context.res = { body: { text:'2021-05-07 0'},  };};
+module.exports = async function (context, req) {  context.res = { body: { text:'2021-05-07 00'},  };};
